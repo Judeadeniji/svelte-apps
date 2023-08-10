7c4e0c22c9a39a6dbe565fc3a8c90cf1614ab47b
Judeadeniji <judeadeniji66@gmail.com>
2023-08-10 18:47:31 +0100

    updated svelte config

--- the-lazy.dev/svelte.config.js
+++ the-lazy.dev/svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    handleHttpError: ({ path, referrer, message }) => {
+      if (path.includes('static')) return;
+      
+      throw new Error(message);
+    }
   },
   preprocess: vitePreprocess()
 };
